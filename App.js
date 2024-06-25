e4f2469d5070977a0c16e3aed69d6e4adf4d999d
ankit singh <ankitsingh@ankits-Air.lan>
2024-06-25 17:28:12 +0530

    Igniting our app -use of parcel, gitignore, node module, dist and parcel-cache

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
 const heading = React.createElement(
     "h1",
     {
@@ -77,8 +80,8 @@ const parentWithSiblingDivs = React.createElement(
         "div",
         { id: "child" },
         [
-            React.createElement("h1", {}, "I am h1 tag"),
-            React.createElement("h2", {}, "I am h2 tag")
+            React.createElement("h1", {}, "Namste React!"),
+            React.createElement("h2", {}, "By Ankit Singh")
         ]
     ),
     React.createElement(
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Namste React
+
+# parcel
+- Dev Build
+- Local Servcer
+- HMR = Hot Module Replacement by using File Watching Algorithm written in C++
+- Caching - Faster build 
+- Image Optimization
+- Minification
+- Bundling
+- Compress
+- Content Hashing
+- Code Splitting
+- Differential Bundling - Support older Browser(Running on diff version of Browsers)
+- Dignostic
+- Error Handling
+- Provides way to host the app on HTTPs
+- Tree Shaking - remove unused code for better performance
+- Different Dev and Prod bundle
